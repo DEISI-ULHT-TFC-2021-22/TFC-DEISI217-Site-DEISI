686d080c041b91bd5883f236969b8a87d7983951
borisdvlpr <eduffmiranda@gmail.com>
2022-07-01 23:21:28 +0100

    WIP - Final TFC Delivery. Updated back-end and front-end apps.

--- deisi_backend/src/main/kotlin/pt/ulusofona/tfc/trabalho/repository/StudentRepository.kt
+++
@@ -1,9 +0,0 @@
-package pt.ulusofona.tfc.trabalho.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import pt.ulusofona.tfc.trabalho.dao.Student
-
-interface StudentRepository: JpaRepository<Student, Long> {
-
-    fun findByAge(age: Int): List<Student>
-}
